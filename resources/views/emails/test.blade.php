bb9c7f0e49364bd59f86690bbcc2a16d7d8c445e
Zia Khan <ziakhan421@gmail.com>
2023-05-16 19:11:53 +0500

    git add

--- resources/views/emails/test.blade.php
+++ resources/views/emails/test.blade.php
@@ -18,6 +18,8 @@
     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 <strong>Thank you Sir. :)</strong>
 </body>
 
