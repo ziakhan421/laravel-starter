77352594d9442fb39c075d1016b8262022768849
Zia Khan <ziakhan421@gmail.com>
2023-05-18 23:54:27 +0500

    Update test.blade.php

--- resources/views/emails/test.blade.php
+++ resources/views/emails/test.blade.php
@@ -12,7 +12,7 @@
     </h2>
 </center>
 <p>Hi, {{$data['name']}}</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+<p>Lorem ipsum dolor sit amet, consectetur , sed do eiusmod
     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
