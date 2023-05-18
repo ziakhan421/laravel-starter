3c0978d1132d3fe4720c0228144a1c35acb083d6
Zia Khan <ziakhan421@gmail.com>
2023-05-19 00:22:29 +0500

    Update bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,4 @@
 import _ from 'lodash';
-
 window._ = _;
 
 /**
@@ -9,7 +8,6 @@ window._ = _;
  */
 
 import axios from 'axios';
-
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
