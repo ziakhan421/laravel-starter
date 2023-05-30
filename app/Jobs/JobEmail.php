e58f6fe83849e47344b5a08c620f2f4d97e1a672
Zia Khan <ziakhan421@gmail.com>
2023-05-30 22:20:55 +0500

    Update JobEmail.php

--- app/Jobs/JobEmail.php
+++ app/Jobs/JobEmail.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Mail;
+use Illuminate\Support\Facades\Mail;
 
 class JobEmail implements ShouldQueue
 {
