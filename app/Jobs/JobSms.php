cb96a7db138b1a4289f2cdba5d818a592d422c8c
Zia Khan <ziakhan421@gmail.com>
2023-05-30 22:21:42 +0500

    Update JobSms.php

--- app/Jobs/JobSms.php
+++ app/Jobs/JobSms.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Facades\SMSGateway;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -33,6 +34,6 @@ class JobSms implements ShouldQueue
      */
     public function handle()
     {
-        \App\Facades\SMSGateway::sendSMS($this->phone, $this->textSms);
+        SMSGateway::sendSMS($this->phone, $this->textSms);
     }
 }
