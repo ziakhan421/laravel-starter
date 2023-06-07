4262c6380556c6f6321503e7048d3628c8afef8c
Zia Khan <ziakhan421@gmail.com>
2023-06-07 22:29:10 +0500

    Update otp.blade.php

--- resources/views/emails/otp.blade.php
+++ resources/views/emails/otp.blade.php
@@ -13,7 +13,7 @@
         </div>
 
         <p style="font-size:1.1em">Hi,</p>
-        <p>Thank you for choosing .... Use the following OTP to complete your Sign Up procedures. OTP is valid for 5
+        <p>Thank you for choosing .. Use the following OTP to complete your Sign Up procedures. OTP is valid for 5
             minutes</p>
         <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;"> {{$data['otp']}}</h2>
         <p style="font-size:0.9em;">Regards,<br/>Test</p>
